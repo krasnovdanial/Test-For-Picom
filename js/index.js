efcad2ee1d119c9a03b6747496da94ad6d03094f
Danial Krasnov <58078456+krasnovdanial@users.noreply.github.com>
2020-07-18 15:40:10 +0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// let passwordInput = document.querySelector('#password');
+// let emailInput = document.querySelector('#email');
+
+// emailInput.addEventListener('change', emailInputBorder)
+
+// function emailInputBorder() {
+//     if (emailInput.value !== '') {
+//         emailInput.style.borderStyle = 'none none solid none';
+//     } else {
+//         emailInput.style.borderStyle = 'solid solid solid solid';
+//     }
+// }
+// passwordInput.addEventListener('change', passwordInputBorder)
+
+// function passwordInputBorder() {
+//     if (passwordInput.value !== '') {
+//         passwordInput.style.borderStyle = 'none none solid none';
+//     } else {
+//         passwordInput.style.borderStyle = 'solid solid solid solid';
+//     }
+// }
+// [...1, 2, 3, 5, 8, 13];
